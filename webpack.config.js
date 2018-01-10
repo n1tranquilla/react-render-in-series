74fb1728ac5d8c2fc443992458b729e42d5343bc
= <=>
2018-01-10 11:16:10 -0400

    use babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  entry: './src/index.js',
+  entry: ['babel-polyfill','./src/index.js'],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
