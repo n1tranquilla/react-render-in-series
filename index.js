c260886529295d0730146fd81a54d77d4442cb28
Nathan Tranquilla <n1tranquilla@gmail.com>
2018-01-17 12:17:08 -0400

    use parcel instead of webpack

---
+++ index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const timeout = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+class RenderInSeries extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state={
+      index:0
+    }
+  }
+
+  async componentDidMount(){
+    const { timeline } = this.props
+  
+    for (let i=0; i<timeline.length; i++){
+      await timeout(timeline[i])
+      this.setState({index: this.state.index+1})
+    }
+  }
+
+  render(){
+    const { index } = this.state
+
+    if(!this.props.children[index]) return null
+
+    return this.props.children[index]
+  }
+} 
+
+RenderInSeries.propTypes={
+  children: PropTypes.array.isRequired,
+  timeline: PropTypes.array.isRequired,
+}
+
+RenderInSeries.defaultProps={
+  children:[],
+  timeline:[]
+}
+
+export default RenderInSeries
